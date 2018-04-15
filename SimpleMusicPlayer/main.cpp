7d103f35a9ce90663e573ae8fa35ca9160c4af77
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-04-15 13:46:43 +0900

    add mp3 load func

--- SimpleMusicPlayer/main.cpp
+++ SimpleMusicPlayer/main.cpp
@@ -2,6 +2,10 @@
 
 #include <opencv2/opencv.hpp>
 #include <string>
+#include <array>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
 
 // One (and only one) of your C++ files must define CVUI_IMPLEMENTATION
 // before the inclusion of cvui.h to ensure its implementaiton is compiled.
@@ -12,6 +16,44 @@
 #pragma comment(linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"")
 
 constexpr const char* WindowName = "Simple Music Player";
+
+constexpr int MaterialMax = 99;
+
+namespace {
+
+	//各種素材ファイル確認関数
+	bool CheckMaterialExistence(const std::string& FilePath) noexcept {
+		std::ifstream Material(FilePath, std::ios_base::in);
+		return Material.is_open();
+	}
+
+	//各種素材ファイルパス処理
+	std::string MaterialPathCalc(const std::int32_t& i, const std::string& FilePath, const std::string& FileFormat) noexcept {
+		std::ostringstream Num;
+
+		Num << std::setfill('0') << std::setw(2) << i + 1;
+
+		return (FilePath + Num.str() + FileFormat);
+	}
+
+	//各種素材読込テンプレート関数
+	template <typename T, typename Func>
+	void MaterialLoadTemplate(std::array<T, MaterialMax>& Material, const std::string& FilePath, const std::string& FileFormat, Func&& Loader) noexcept {
+		for (std::int32_t i = 0; i < MaterialMax; i++) {
+			if (CheckMaterialExistence(MaterialPathCalc(i, FilePath, FileFormat)))
+				Material[i] = Loader(MaterialPathCalc(i, FilePath, FileFormat));
+		}
+	}
+}
+
+std::array<int, MaterialMax> LoadMusic() {
+
+	std::array<int, MaterialMax> MusicContainer;
+
+	MaterialLoadTemplate(MusicContainer, "mp3/bgm", ".mp3", [](const std::string& Path) {return DxLib::LoadSoundMem(Path.c_str()); });
+
+	return MusicContainer;
+}
 
 void DrawButton(cv::Mat frame) {
 
@@ -50,6 +92,8 @@ int main(int argc, const char *argv[])
 	cvui::init(WindowName);
 
 	double value = 100.0;
+
+	const std::array<int, MaterialMax> MusicContainer = LoadMusic();
 
 	while (true) {
 		// Fill the frame with a nice color
