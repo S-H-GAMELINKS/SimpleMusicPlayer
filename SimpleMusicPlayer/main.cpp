9baeea4ad3bf94211513b136785075b632107592
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-04-20 22:28:04 +0900

    remove Material load

--- SimpleMusicPlayer/main.cpp
+++ SimpleMusicPlayer/main.cpp
@@ -56,34 +56,6 @@ std::array<std::string, MaterialMax> AllFilePath() {
 	return Container;
 }
 
-// music material load func's
-namespace {
-
-	//各種素材ファイル確認関数
-	bool CheckMaterialExistence(const std::string& FilePath) noexcept {
-		std::ifstream Material(FilePath, std::ios_base::in);
-		return Material.is_open();
-	}
-
-	//各種素材ファイルパス処理
-	std::string MaterialPathCalc(const std::int32_t& i, const std::string& FilePath, const std::string& FileFormat) noexcept {
-		std::ostringstream Num;
-
-		Num << std::setfill('0') << std::setw(2) << i + 1;
-
-		return (FilePath + Num.str() + FileFormat);
-	}
-
-	//各種素材読込テンプレート関数
-	template <typename T, typename Func>
-	void MaterialLoadTemplate(std::array<T, MaterialMax>& Material, const std::string& FilePath, const std::string& FileFormat, Func&& Loader) noexcept {
-		for (std::int32_t i = 0; i < MaterialMax; i++) {
-			if (CheckMaterialExistence(MaterialPathCalc(i, FilePath, FileFormat)))
-				Material[i] = Loader(MaterialPathCalc(i, FilePath, FileFormat));
-		}
-	}
-}
-
 // loading music
 std::array<int, MaterialMax> LoadMusic(const std::array<std::string, MaterialMax> Container) {
 
